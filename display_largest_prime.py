7f2759b8e72a7588a55e8ce3f4137bf9ecd264d6
alexglandon <77987885+alexglandon@users.noreply.github.com>
2024-12-26 18:12:17 -0500

    Update display_largest_prime.py

--- display_largest_prime.py
+++ display_largest_prime.py
@@ -5,7 +5,7 @@ import shutil
 
 def test_mersenne_n(
     n, mersenne_prime_file
-)
+):
 
     #note
     #(2^n)-1=2^0+2^1+...+2^(n-1)
@@ -20,9 +20,9 @@ def test_mersenne_n(
     binary_power_end+1):
 
         print(
-            “test”,
+            "test",
             binary_power,
-            “of”,
+            "of",
             binary_power_end)
 
         #write binary power to temp_2.txt
@@ -36,9 +36,9 @@ def test_mersenne_n(
             add_2=temp_2.txt,
             sum=temp_3.txt)
 
-        os.remove(“temp_1.txt”)
-        os.remove(“temp_2.txt”)
-        os.rename(“temp_3.txt”, “temp_1.txt”)
+        os.remove("temp_1.txt")
+        os.remove("temp_2.txt")
+        os.rename("temp_3.txt", "temp_1.txt")
    
     verify_equality(
         temp_1.txt,
@@ -51,17 +51,17 @@ def write_2_to_the_m(
 ):
     
     #write 2 (2^1) to temp_4.txt
-    with open(“temp_4.txt”, "w") as f:
+    with open("temp_4.txt", "w") as f:
         f.write("1")
 
     for multiplication in range(m):
         times_2(
-            in_file=“temp_4.txt”,
+            in_file="temp_4.txt",
             out_file=result_file
         )
 
-        os.remove(“temp_4.txt”)
-        os.rename(result_file, “temp_4.txt”)
+        os.remove("temp_4.txt")
+        os.rename(result_file, "temp_4.txt")
 
 
 def add_files(
@@ -72,52 +72,52 @@ def add_files(
     with open(add_smaller_file, "r") as f:
         digit=f.read()
         small_count = 1
-        while digit >= ‘0’ and digit <= ‘9’:
+        while digit >= '0' and digit <= '9':
             digit = f.read(1)
             small_count += 1
 
     with open(add_larger_file, "r") as f:
         digit=f.read()
         large_count = 1
-        while digit >= ‘0’ and digit <= ‘9’:
+        while digit >= '0' and digit <= '9':
             digit = f.read(1)
             large_count += 1
 
-    with open(add_smaller_file, “a”) as f:
+    with open(add_smaller_file, "a") as f:
         for difference in \
         range(large_count-small_count):
 
-        f.write(“0”)
+            f.write("0")
 
     with \
-        open(add_smaller_file”, “r”) as f,
-        open(add_larger_file, “r”) as g,
-        open(sum_file, “w”) as h:
+        open(add_smaller_file, "r") as f, \
+        open(add_larger_file, "r") as g, \
+        open(sum_file, "w") as h:
 
-        a = f.read()
-        b = f.read()
+        a = f.read(1)
+        b = g.read(1)
         r = 0
         
-        while a >= ‘0’ and a <= ‘9’:
+        while a >= '0' and a <= '9':
         
             c = int(a)+int(b)+r
             r=c//10
             c=c%10
             h.write(str(c))
 
-       h.write(str(r))
+        h.write(str(r))
 
 
 def times_2(in_file, out_file):
     
-    shutil.copy(in_file, “temp_6.txt”)
+    shutil.copy(in_file, "temp_6.txt")
 
     add_files(
             add_smaller_file=in_file,
-            add_larger_file=“temp_6.txt”,
-            sum_file=out_file):
+            add_larger_file="temp_6.txt",
+            sum_file=out_file)
 
-    os.remove(“temp_6.txt”)
+    os.remove("temp_6.txt")
 
 
 def verify_equality(
@@ -125,21 +125,21 @@ def verify_equality(
         merseene_prime_file):
 
     with \
-        open(test_file”, “r”) as f,
-        open(mersenne_prime_file, “r”) as g:
+        open(test_file, "r") as f, \
+        open(mersenne_prime_file, "r") as g:
 
-        a = f.read()
-        b = g.read()
+        a = f.read(1)
+        b = g.read(1)
 
-       passed = True
+        passed = True
         
-        while a >= ‘0’ and a <= ‘9’:
+        while a >= '0' and a <= '9':
         
             if a!=b:
                 passed = False
                 break
 
-        print(“Test result”, passed_test)
+        print("Test result", passed_test)
 
 
 
@@ -148,36 +148,36 @@ def mersenne_prime_write(n):
     
     #using 1x2x2…
     write_2_to_the_m(
-        “merseene_prime_file.txt”,n)
+        "merseene_prime_file.txt",n)
 
-    subtract_one(“merseene_prime_file.txt”)
+    subtract_one("merseene_prime_file.txt")
 
 
 def subtract_one(subtract_file):
 
-with \
-    open(subtract_file, "r") as f,
-    open(“temp_file_7.txt”, “w”) as g:
+    with \
+    open(subtract_file, "r") as f, \
+    open("temp_file_7.txt", "w") as g:
 
         digit=f.read()
         g.write(str(int(digit)-1))
-        while digit >= ‘0’ and digit <= ‘9’:
-            digit = f.read()
+        while digit >= '0' and digit <= '9':
+            digit = f.read(1)
             g.write(digit)
 
 def mersenne_prime_display():
     
-    with open(“mersenne_prime_file.txt”, “r”) as g:
+    with open("mersenne_prime_file.txt", "r") as g:
     
         with \
-        open(“large-prime-number.html”, "r") as f:
+        open("large-prime-number.html", "r") as f:
 
-            f.write(“<html>\n<head><title>Large Prime Number<\title>\n<\head><body>\ndecimal digits of largest known prime number:<br><br>\n”
+            f.write("<html>\n<head><title>Large Prime Number<\title>\n<\head><body>\ndecimal digits of largest known prime number:<br><br>\n"
 )
 
             digit_count = 0
             line_count = 0
-            file _count = 1
+            file_count = 1
             file.seek(0, 2)  # Go to the end of the file
             position = file.tell()  # end of file
 
@@ -188,21 +188,21 @@ def mersenne_prime_display():
                 g.write(digit)
                 digit_count += 1
                 if digit_count == 100:
-                    g.write(“<br>\n”)
+                    g.write("<br>\n")
                     digit_count = 0
                     line_count += 1
                 if line_count == 1000:
-                    f.write(“<br><br>\n<a href=\“large-prime-page-”+str(file_count+1)+“.html\“>\n<\body>\n<\html>”
+                    f.write("<br><br>\n<a href=\"large-prime-page-"+str(file_count+1)+".html\">\n<\body>\n<\html>")
                     line_count = 0
                     file_count += 1
                     break
 
         while position > 0:
             with \
-            open(“large-prime-number-page”\
-            +str(file_count)+“.html”, "r") as f:
+            open("large-prime-number-page"\
+            +str(file_count)+".html", "r") as f:
 
-                f.write(“<html>\n<head><title>Large Prime Number - Page ”+str(file_count)+“<\title>\n<\head><body>\ndecimal digits of largest known prime number:<br><br>\n”
+                f.write("<html>\n<head><title>Large Prime Number - Page "+str(file_count)+"<\title>\n<\head><body>\ndecimal digits of largest known prime number:<br><br>\n"
 )
 
                 while position > 0:
@@ -212,19 +212,23 @@ def mersenne_prime_display():
                     digit = file.read(1)
                     g.write(digit)
                     digit_count += 1
-                   if digit_count == 100:
-                       g.write(“<br>\n”)
+                    if digit_count == 100:
+                       g.write("<br>\n")
                        digit_count = 0
                        line_count += 1
-                        if line_count == 1000:
-                            f.write(“<br><br>\n<a href=\“large-prime-page-”+str(file_count+1)+“.html\“>\n<\body>\n<\html>”
+                       if line_count == 10000:
+                            f.write("<br><br>\n<a href=\"large-prime-page-"+str(file_count+1)+".html\">")
                             line_count = 0
                             file_count += 1
                             break
 
+        for file_index in range(file_count):
+            f.write("of "+str(file_count)+"\n<\body>\n<\html>")
 
-        f.write(“\n<\body>\n<\html>”
 
-        
+
+mersenne_prime_write(136279841)
+
+test_mersenne_n(136279841,"merseene_prime_file.txt")
 
     
