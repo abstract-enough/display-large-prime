1de3930f7093ebfaa4e8193f3af1335d56c0290d
aglan001@odu.edu <aglan001@odu.edu>
2025-01-05 22:39:46 -0500

    html edits

--- display_largest_prime.py
+++ display_largest_prime.py
@@ -35,65 +35,6 @@ def mersenne_prime_write(n):
     return merseene_prime
 
 
-def mersenne_prime_display(merseene_prime_list):
-
-
-
-    with \
-    open("large-prime-number.html", "r") as f:
-
-        f.write("<html>\n<head><title>Large Prime Number<\title>\n<\head><body>\ndecimal digits of largest known prime number:<br><br>\n"
-)
-
-        digit_count = 0
-        line_count = 0
-
-        i = len(merseene_prime_list)-1
-        while i >= 0:
-
-
-            digit = merseene_prime_list[i]
-            i -= 1
-            f.write(digit)
-            digit_count += 1
-            if digit_count == 100:
-                f.write("<br>\n")
-                digit_count = 0
-                line_count += 1
-            if line_count == 10000:
-                f.write("<br><br>\n<a href=\"large-prime-page-"+str(file_count+1)+".html\">\n<\body>\n<\html>")
-                line_count = 0
-                file_count += 1
-                break
-
-
-        with \
-        open("large-prime-number-page"\
-        +str(file_count)+".html", "r") as f:
-
-            f.write("<html>\n<head><title>Large Prime Number - Page "+str(file_count)+"<\title>\n<\head><body>\ndecimal digits of largest known prime number:<br><br>\n"
-            )
-
-            while i >= 0:
-
-                digit = merseene_prime_list[i]
-                i -= 1
-
-                f.write(digit)
-                digit_count += 1
-                if digit_count == 100:
-                    f.write("<br>\n")
-                    digit_count = 0
-                    line_count += 1
-                    if line_count == 10000:
-                        f.write("<br><br>\n<a href=\"large-prime-page-"+str(file_count+1)+".html\">")
-                        line_count = 0
-                        file_count += 1
-                        break
-
-        for file_index in range(file_count):
-            f.write("of "+str(file_count)+"\n<\body>\n<\html>")
-
 
 
 merseene_prime = mersenne_prime_write(136279841)
--- prime_html.py
+++ prime_html.py
@@ -6,26 +6,26 @@ def mersenne_prime_display(merseene_prime_list):
     with \
     open("large-prime-number.html", "w") as f:
 
-        f.write("<html>\n<head><title>Large Prime Number<\title>\n<\head><body>\ndecimal digits of largest known prime number:<br><br>\n"
-)
+        f.write("<html>\n<head><title>Large Prime Number</title>\n</head><body>\ndecimal digits of largest known prime number:<br><br>\n")
 
         digit_count = 0
         line_count = 0
+        file_count = 1
 
-        i = len(merseene_prime_list)-1
-        while i >= 0:
+        i = 0
+        while i < len(merseene_prime_list) :
 
 
             digit = merseene_prime_list[i]
-            i -= 1
+            i = i + 1
             f.write(digit)
             digit_count += 1
             if digit_count == 100:
                 f.write("<br>\n")
                 digit_count = 0
                 line_count += 1
-            if line_count == 10000:
-                f.write("<br><br>\n<a href=\"large-prime-page-"+str(file_count+1)+".html\">\n<\body>\n<\html>")
+            if line_count == 100000:
+                f.write("<br><br>\n<a href=\"large-prime-number-page-"+str(file_count+1)+".html\">\n</body>\n</html>")
                 line_count = 0
                 file_count += 1
                 break
@@ -35,13 +35,12 @@ def mersenne_prime_display(merseene_prime_list):
         open("large-prime-number-page"\
         +str(file_count)+".html", "w") as f:
 
-            f.write("<html>\n<head><title>Large Prime Number - Page "+str(file_count)+"<\title>\n<\head><body>\ndecimal digits of largest known prime number:<br><br>\n"
-            )
+            f.write("<html>\n<head><title>Large Prime Number - Page "+str(file_count)+"</title>\n</head><body>\ndecimal digits of largest known prime number:<br><br>\n")
 
-            while i >= 0:
+            while i < len(merseene_prime_list):
 
                 digit = merseene_prime_list[i]
-                i -= 1
+                i = i + 1
 
                 f.write(digit)
                 digit_count += 1
@@ -49,19 +48,19 @@ def mersenne_prime_display(merseene_prime_list):
                     f.write("<br>\n")
                     digit_count = 0
                     line_count += 1
-                    if line_count == 10000:
-                        f.write("<br><br>\n<a href=\"large-prime-page-"+str(file_count+1)+".html\">")
-                        line_count = 0
-                        file_count += 1
+                if line_count == 100000:
+                    f.write("<br><br>\n<a href=\"large-prime-number-page-"+str(file_count+1)+".html\">page "+str(file_count+1))
+                    line_count = 0
+                    file_count += 1
                         break
 
         for file_index in range(file_count):
-            f.write("of "+str(file_count)+"\n<\body>\n<\html>")
+            f.write(" of "+str(file_count)+"\n</body>\n</html>")
 
 
 
 
-with open("large_prime_number.txt", "r") as f:
+with open("largest_known_prime_number.txt", "r") as f:
     prime_string = f.read()
 
 
