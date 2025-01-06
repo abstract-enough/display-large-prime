b82be1f33bea4852c75490b9c3e37d224032818d
aglan001@odu.edu <aglan001@odu.edu>
2025-01-05 23:12:09 -0500

    html done

--- prime_html.py
+++ prime_html.py
@@ -3,8 +3,7 @@ def mersenne_prime_display(merseene_prime_list):
 
 
 
-    with \
-    open("large-prime-number.html", "w") as f:
+    with open("large-prime-number.html", "w") as f:
 
         f.write("<html>\n<head><title>Large Prime Number</title>\n</head><body>\ndecimal digits of largest known prime number:<br><br>\n")
 
@@ -25,39 +24,48 @@ def mersenne_prime_display(merseene_prime_list):
                 digit_count = 0
                 line_count += 1
             if line_count == 100000:
-                f.write("<br><br>\n<a href=\"large-prime-number-page-"+str(file_count+1)+".html\">\n</body>\n</html>")
+                f.write("<br><br>\n<a href=\"large-prime-number-page-"+str(file_count+1)+".html\">page "+str(file_count+1))
                 line_count = 0
                 file_count += 1
                 break
 
+        
+        while True:
 
-        with \
-        open("large-prime-number-page"\
-        +str(file_count)+".html", "w") as f:
+            with open("large-prime-number-page-"+str(file_count)+".html", "w") as f:
 
-            f.write("<html>\n<head><title>Large Prime Number - Page "+str(file_count)+"</title>\n</head><body>\ndecimal digits of largest known prime number:<br><br>\n")
+                f.write("<html>\n<head><title>Large Prime Number - Page "+str(file_count)+"</title>\n</head><body>\ndecimal digits of largest known prime number:<br><br>\n")
 
-            while i < len(merseene_prime_list):
+                while i < len(merseene_prime_list):
 
-                digit = merseene_prime_list[i]
-                i = i + 1
+                    digit = merseene_prime_list[i]
+                    i = i + 1
 
-                f.write(digit)
-                digit_count += 1
-                if digit_count == 100:
-                    f.write("<br>\n")
-                    digit_count = 0
-                    line_count += 1
-                if line_count == 100000:
-                    f.write("<br><br>\n<a href=\"large-prime-number-page-"+str(file_count+1)+".html\">page "+str(file_count+1))
-                    line_count = 0
-                    file_count += 1
+                    f.write(digit)
+                    digit_count += 1
+                    if digit_count == 100:
+                        f.write("<br>\n")
+                        digit_count = 0
+                        line_count += 1
+                    if line_count == 100000:
+                        f.write("<br><br>\n<a href=\"large-prime-number-page-"+str(file_count+1)+".html\">page "+str(file_count+1))
+                        line_count = 0
+                        file_count += 1
                         break
 
-        for file_index in range(file_count):
-            f.write(" of "+str(file_count)+"\n</body>\n</html>")
+                if i == len(merseene_prime_list):
+                    break
+
+    with open("large-prime-number.html", "a") as f:
+        f.write(" of "+str(file_count)+"</a>\n</body>\n</html>")
+
+    for file_index in range(2,file_count):
+        with open("large-prime-number-page-"+str(file_index)+".html", "a") as f:
+            f.write(" of "+str(file_count)+"</a>\n</body>\n</html>")
 
 
+    with open("large-prime-number-page-"+str(file_count)+".html", "a") as f:
+        f.write("<br><br>end</body>\n</html>")
 
 
 with open("largest_known_prime_number.txt", "r") as f:
